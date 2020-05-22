ef98860a4468dafb7b7909a4f36d4ba68e0a4299
samuel atobatele <swumatob@yahoo.co.uk>
2020-05-22 15:13:26 +0100

    Mr Cock-tail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "/cocktails", to: "cocktails#index"
+  #root to: "cocktails#index"
+  resources :cocktails do
+    resources :doses
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Cocktail.create(name: "Brandy Alexander")
+Cocktail.create(name: "Blood & Sand")
+Cocktail.create(name: "Sidecar")
+Dose.create(description: "6 leaves")
+Dose.create(description: "1.5 oz")
+Dose.create(description: "4.5 cl")
